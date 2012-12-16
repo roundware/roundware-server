5fe72e6d8da57b75187ba878fcc7546e84071e52
Jule Slootbeek <jslootbeek@gmail.com>
2012-12-16 14:27:13 -0500

    Ability to deactivate notifications on a model or action level.

--- roundware/notifications/__init__.py
+++ roundware/notifications/__init__.py
@@ -7,41 +7,46 @@ from roundware.notifications.models import ActionNotification, ENABLED_MODELS
 __author__ = 'jule'
 logger = logging.getLogger("notifications")
 
-def send_notifications_add(sender, instance, created, **kwargs):
-    if not created:
-        return
+def send_notifications_add_edit(sender, instance, created, **kwargs):
     object_string = sender._meta.object_name.lower()
     logger.info("caught add %s", object_string)
     objects = [i[0] for i in ENABLED_MODELS if i[1].lower() == object_string]
     if objects:
+        action = 0 if not created else 1
         logger.info("%s %s", object_string, instance.id)
         object_int = objects[0]
         date_diff = datetime.datetime.now() - datetime.timedelta(seconds=getattr(settings, "NOTIFICATIONS_TIME_BETWEEN", 30))
         notifications = ActionNotification.objects.filter(notification__model = object_int,
-                                                          action = 0,
-                                                          )
-        logger.info("%s", notifications)
-        for n in notifications:
-            if n.last_sent_reference != instance.pk or (n.last_sent_reference == instance.pk and n.last_sent_time < date_diff):
-                n.notify(ref=instance.pk)
-
-def send_notifications_edit(sender, instance, created, **kwargs):
-    if created:
-        return
-    object_string = sender._meta.object_name.lower()
-    logger.info("caught edit %s", object_string)
-    objects = [i[0] for i in ENABLED_MODELS if i[1].lower() == object_string]
-    if objects:
-        logger.info("%s %s", object_string, instance.id)
-        object_int = objects[0]
-        date_diff = datetime.datetime.now() - datetime.timedelta(seconds=getattr(settings, "NOTIFICATIONS_TIME_BETWEEN", 30))
-        notifications = ActionNotification.objects.filter(notification__model = object_int,
-                                                          action = 1,
+                                                          notification__active = True,
+                                                          action = action,
+                                                          active = True,
                                                          )
         logger.info("%s", notifications)
         for n in notifications:
             if n.last_sent_reference != instance.pk or (n.last_sent_reference == instance.pk and n.last_sent_time < date_diff):
                 n.notify(ref=instance.pk)
+
+#def send_notifications_edit(sender, instance, created, **kwargs):
+#    #don't trigger when saving a new object
+#    if created:
+#        return
+#    #get the actual
+#    object_string = sender._meta.object_name.lower()
+#    logger.info("caught edit %s", object_string)
+#    objects = [i[0] for i in ENABLED_MODELS if i[1].lower() == object_string]
+#    if objects:
+#        logger.info("%s %s", object_string, instance.id)
+#        object_int = objects[0]
+#        date_diff = datetime.datetime.now() - datetime.timedelta(seconds=getattr(settings, "NOTIFICATIONS_TIME_BETWEEN", 30))
+#        notifications = ActionNotification.objects.filter(notification__model = object_int,
+#                                                          notification__active = True,
+#                                                          action = 1,
+#                                                          active = True,
+#                                                         )
+#        logger.info("%s", notifications)
+#        for n in notifications:
+#            if n.last_sent_reference != instance.pk or (n.last_sent_reference == instance.pk and n.last_sent_time < date_diff):
+#                n.notify(ref=instance.pk)
 
 def send_notifications_delete(sender, instance, **kwargs):
     object_string = sender._meta.object_name.lower()
@@ -51,11 +56,13 @@ def send_notifications_delete(sender, instance, **kwargs):
         logger.info("%s %s", object_string, instance.id)
         object_int = objects[0]
         notifications = ActionNotification.objects.filter(notification__model = object_int,
-                                                          action = 2)
+                                                          notification__active = True,
+                                                          action = 2,
+                                                          active = True,
+                                                         )
         logger.info("%s", notifications)
         for n in notifications:
             n.notify(ref=instance.pk)
 
-post_save.connect(send_notifications_add)
+post_save.connect(send_notifications_add_edit)
 post_delete.connect(send_notifications_delete)
-post_save.connect(send_notifications_edit)
