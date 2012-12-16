c4c7be7dad48afbdffb1ad4a199cd2bc76d68e9a
Jule Slootbeek <jslootbeek@gmail.com>
2012-12-16 15:08:40 -0500

    if an object is edited within 2 seconds of it being added, don't send a notification as it is likely caused by a save() call in the objects creation.

--- roundware/notifications/__init__.py
+++ roundware/notifications/__init__.py
@@ -7,48 +7,43 @@ from roundware.notifications.models import ActionNotification, ENABLED_MODELS
 __author__ = 'jule'
 logger = logging.getLogger("notifications")
 
-def send_notifications_add_edit(sender, instance, created, **kwargs):
+def send_notifications_add_edit(sender, instance, created, *args, **kwargs):
+    #get the type of model from the sender
     object_string = sender._meta.object_name.lower()
-    logger.info("caught add %s", object_string)
+    logger.info("caught add or edit %s, created: %s" % (object_string, created))
+    #check whether the model is represented as being able to handle notifications
     objects = [i[0] for i in ENABLED_MODELS if i[1].lower() == object_string]
     if objects:
-        action = 0 if not created else 1
+        #0 = add
+        #1 = edit
+        action = 0 if created else 1
         logger.info("%s %s", object_string, instance.id)
         object_int = objects[0]
+        #find the time between this notifications and the last time this notification was sent
         date_diff = datetime.datetime.now() - datetime.timedelta(seconds=getattr(settings, "NOTIFICATIONS_TIME_BETWEEN", 30))
+        #get the enabled notifications
         notifications = ActionNotification.objects.filter(notification__model = object_int,
                                                           notification__active = True,
                                                           action = action,
                                                           active = True,
                                                          )
         logger.info("%s", notifications)
+        #loop through and execute them
         for n in notifications:
-            if n.last_sent_reference != instance.pk or (n.last_sent_reference == instance.pk and n.last_sent_time < date_diff):
-                n.notify(ref=instance.pk)
+            #only execute notification if we're working with a different object
+            #or enough time has past since the last notification
+            if n.last_sent_reference != instance.id or (n.last_sent_reference == instance.id and n.last_sent_time < date_diff):
+                #if an edit is caught, make sure that the object wasn't created within 2 seconds
+                #this is to stop the app from sending two notifications when the save() method is called
+                #during the object creation process.
+                if action == 1 and ActionNotification.objects.filter(notification__model = object_int,
+                                                                     last_sent_reference = instance.pk,
+                                                                     last_sent_time__gte=datetime.datetime.now() - datetime.timedelta(seconds=2),
+                                                                     action = 0):
+                    return
+                n.notify(ref=instance.id)
 
-#def send_notifications_edit(sender, instance, created, **kwargs):
-#    #don't trigger when saving a new object
-#    if created:
-#        return
-#    #get the actual
-#    object_string = sender._meta.object_name.lower()
-#    logger.info("caught edit %s", object_string)
-#    objects = [i[0] for i in ENABLED_MODELS if i[1].lower() == object_string]
-#    if objects:
-#        logger.info("%s %s", object_string, instance.id)
-#        object_int = objects[0]
-#        date_diff = datetime.datetime.now() - datetime.timedelta(seconds=getattr(settings, "NOTIFICATIONS_TIME_BETWEEN", 30))
-#        notifications = ActionNotification.objects.filter(notification__model = object_int,
-#                                                          notification__active = True,
-#                                                          action = 1,
-#                                                          active = True,
-#                                                         )
-#        logger.info("%s", notifications)
-#        for n in notifications:
-#            if n.last_sent_reference != instance.pk or (n.last_sent_reference == instance.pk and n.last_sent_time < date_diff):
-#                n.notify(ref=instance.pk)
-
-def send_notifications_delete(sender, instance, **kwargs):
+def send_notifications_delete(sender, instance, *args, **kwargs):
     object_string = sender._meta.object_name.lower()
     logger.info("caught delete %s", object_string)
     objects = [i[0] for i in ENABLED_MODELS if i[1].lower() == object_string]
