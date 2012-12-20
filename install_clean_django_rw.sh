ff795bfea3d517ea8880936f295186fb9c9bb2ec
Halsey Burgund <halsey@halseyburgund.com>
2012-12-19 22:40:10 -0500

    add South migrations directories to install script

--- install_clean_django_rw.sh
+++ install_clean_django_rw.sh
@@ -7,3 +7,5 @@ sudo rm -rf $DIST_PATH/roundware/
 sudo python $FILE_HOME/setup.py install
 sudo cp -r $CODE_PATH/roundware/rw/templates $DIST_PATH/roundware/rw/
 sudo cp -r $CODE_PATH/roundware/geoposition/templates $DIST_PATH/roundware/geoposition/
+sudo cp -r $CODE_PATH/roundware/notifications/migrations $DIST_PATH/roundware/notifications/migrations
+sudo cp -r $CODE_PATH/roundware/rw/migrations $DIST_PATH/roundware/rw/migrations
