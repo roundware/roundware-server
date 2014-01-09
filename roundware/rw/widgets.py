e38d18ad629a0535ade72144565cdd78aea430ed
Bryan Wilson <bryanlandia@gmail.com>
2014-01-09 21:09:10 +0000

    add green plus icon to add another link below localized text for batch tags add form

--- roundware/rw/widgets.py
+++ roundware/rw/widgets.py
@@ -32,7 +32,7 @@ class NonAdminRelatedFieldWidgetWrapper(RelatedFieldWidgetWrapper):
             output.append(u'<br/><a href="%s" class="add-another" id="add_id_%s" '
                           'onclick="return showAddAnotherPopup(this);"> ' % 
                           (self.add_url, name))
-            output.append(u'<img src="%simg/admin/icon_addlink.gif" width="10"'
-                          ' height="10" alt="%s"/></a>' % 
-            (settings.ADMIN_MEDIA_PREFIX, _('Add Another')))
+            output.append(u'<img src="%simg/icon_addlink.gif" width="10"'
+                          ' height="10" alt="%s"/>&nbsp;&nbsp;%s</a>' % 
+            (settings.ADMIN_MEDIA_PREFIX, _('Add Another'), _('Add Another')))
         return mark_safe(u''.join(output))
