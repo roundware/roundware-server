c9487e8dc656ca768000773ed6d9b49755532d77
Brad Erickson <eosrei@gmail.com>
2014-08-15 16:22:25 -0700

    Removing gstreamer0.10-gnomevfs and multiverse repo/package from install.sh

--- files/home-user-runserver.sh
+++ files/home-user-runserver.sh
@@ -2,4 +2,5 @@
 # Run the Django development server for Roundware on port 8888
 pip install -r ~/roundware-server/requirements/dev.txt
 ~/roundware-server/roundware/manage.py runserver 0.0.0.0:8888 --settings=roundware.settings.dev
-
+# Kill off the rwstreamd.py scripts
+killall python
