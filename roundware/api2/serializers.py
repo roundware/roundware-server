8d581a169aef7e6372dd319c4da703fa063ab866
Gray Bowman <gray.bowman@gmail.com>
2015-02-18 10:19:59 -0500

    Commented out unreviewed code.  Updated Sessions handling.

--- roundware/api2/serializers.py
+++ roundware/api2/serializers.py
@@ -3,7 +3,7 @@
 
 # The Django REST Framework object serializers for the V2 API.
 from __future__ import unicode_literals
-from roundware.rw.models import Asset, Event, ListeningHistoryItem, Project, Tag, Session
+from roundware.rw.models import Asset, Event, Language, ListeningHistoryItem, Project, Tag, Session
 from roundware.lib.api import get_project_tags
 from rest_framework import serializers
 from rest_framework.serializers import ValidationError
@@ -13,75 +13,96 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class AssetSerializer(serializers.ModelSerializer):
-    audiolength_in_seconds = serializers.FloatField()
+# class AssetSerializer(serializers.ModelSerializer):
+#     audiolength_in_seconds = serializers.FloatField()
 
-    class Meta:
-        model = Asset
+#     class Meta:
+#         model = Asset
 
 
-class EventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
+# class EventSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Event
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Project
 
 
-class ListenEventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ListeningHistoryItem
+# class ListenEventSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ListeningHistoryItem
 
 
 class SessionSerializer(serializers.ModelSerializer):
+    starttime = serializers.DateTimeField(required=True)
+    language = serializers.CharField(max_length=2, default="en")
+    client_system = serializers.CharField(max_length=128, required=True)
+
     class Meta:
         model = Session
 
+    def validate_language(self, value):
+        try:
+            Language.objects.get(language_code=value)
+        except Language.DoesNotExist:
+            raise ValidationError("Language Code %s not found" % value)
+        return value
 
-class StreamSerializer(serializers.Serializer):
-    session_id = serializers.IntegerField()
-    project_id = serializers.IntegerField(required=False)
-    tags = serializers.CharField(max_length=255, required=False)
-
-    lat = serializers.FloatField(required=False)
-    long = serializers.FloatField(required=False)
-    url = serializers.URLField(required=False)
-
-    def validate_session_id(self, attrs, source):
-        """
-        Check the required session_id field.
-        """
-        session_id = attrs[source]
-        if not Session.objects.filter(pk=session_id).exists():
-            raise ValidationError("session_id=%s does not exist." % session_id)
-
-        return attrs
-
-    def validate(self, attrs):
-        """
-        Validate additional Stream data.
-        """
-
-        session = Session.objects.get(pk=attrs['session_id'])
-        project = session.project
-
-        if 'project_id' in attrs and attrs['project_id'] != project.id:
-            raise ValidationError("project_id=%s does not match session.project=%s." %
-                                  (attrs['project_id'], project.id))
-        # Set project_id to make sure it exists.
-        attrs['project_id'] = project.id
-
-        # TODO: Validate tags against available project tags
-        # if 'tags' in attrs:
-        return attrs
+    def create(self, validated_data):
+        lang = Language.objects.get(language_code=validated_data['language'])
+        session = Session.objects.create(device_id=self.context["request"].user.userprofile.device_id,
+                                         project=validated_data["project"],
+                                         starttime=validated_data["starttime"],
+                                         language_id=lang.pk,
+                                         client_type=self.context["request"].user.userprofile.client_type,
+                                         client_system=validated_data["client_system"])
+        session.save()
+        return session
 
 
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
+# class StreamSerializer(serializers.Serializer):
+#     session_id = serializers.IntegerField()
+#     project_id = serializers.IntegerField(required=False)
+#     tags = serializers.CharField(max_length=255, required=False)
+
+#     lat = serializers.FloatField(required=False)
+#     long = serializers.FloatField(required=False)
+#     url = serializers.URLField(required=False)
+
+#     def validate_session_id(self, attrs, source):
+#         """
+#         Check the required session_id field.
+#         """
+#         session_id = attrs[source]
+#         if not Session.objects.filter(pk=session_id).exists():
+#             raise ValidationError("session_id=%s does not exist." % session_id)
+
+#         return attrs
+
+#     def validate(self, attrs):
+#         """
+#         Validate additional Stream data.
+#         """
+
+#         session = Session.objects.get(pk=attrs['session_id'])
+#         project = session.project
+
+#         if 'project_id' in attrs and attrs['project_id'] != project.id:
+#             raise ValidationError("project_id=%s does not match session.project=%s." %
+#                                   (attrs['project_id'], project.id))
+#         # Set project_id to make sure it exists.
+#         attrs['project_id'] = project.id
+
+#         # TODO: Validate tags against available project tags
+#         # if 'tags' in attrs:
+#         return attrs
+
+
+# class TagSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Tag
 
 
 class UserSerializer(serializers.Serializer):
--- roundware/api2/views.py
+++ roundware/api2/views.py
@@ -10,7 +10,7 @@ from roundware.rw.models import (Asset, Event, ListeningHistoryItem, Project,
 from roundware.api2 import serializers
 from roundware.api2.permissions import AuthenticatedReadAdminWrite
 from roundware.lib.api import get_project_tags
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated, DjangoObjectPermissions
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError
@@ -25,112 +25,152 @@ logger = logging.getLogger(__name__)
 # Note: Keep this stuff in alphabetical order!
 
 
-class AssetViewSet(viewsets.ModelViewSet):
-    """
-    API V2: api/2/assets/:asset_id
+# class AssetViewSet(viewsets.ModelViewSet):
+#     """
+#     API V2: api/2/assets/:asset_id
 
+#     <Permissions>
+#     Anonymous: None.
+#     Authenticated: GET/POST. PUT/PATCH/DELETE for objects owned by user.
+#     Admin: GET/POST/PUT/PATCH/DELETE.
+#     """
+
+#     # TODO: Implement DjangoObjectPermissions
+#     queryset = Asset.objects.all()
+#     serializer_class = serializers.AssetSerializer
+#     permission_classes = (IsAuthenticated, DjangoObjectPermissions)
+
+
+# class EventViewSet(viewsets.ModelViewSet):
+#     """
+#     API V2: api/2/events/:event_id
+
+#     <Permissions>
+#     Anonymous: None.
+#     Authenticated: GET/POST
+#     Admin: GET/POST
+#     """
+
+#     # TODO: Implement ViewCreate permission.
+#     queryset = Event.objects.all()
+#     serializer_class = serializers.EventSerializer
+#     permission_classes = (IsAuthenticated,)
+
+
+# class ListenEventViewSet(viewsets.ModelViewSet):
+#     """
+#     API V2: api/2/listenevents/:listenevent_id
+
+#     <Permissions>
+#     Anonymous: None.
+#     Authenticated: GET/POST.
+#     Admin: GET/POST.
+#     """
+
+#     # TODO: Implement ViewCreate permission.
+#     # TODO: Rename ListeningHistoryItem model to ListenEvent.
+#     queryset = ListeningHistoryItem.objects.all()
+#     serializer_class = serializers.ListenEventSerializer
+#     permission_classes = (IsAuthenticated,)
+
+
+class ProjectViewSet(viewsets.ViewSet):
+    """
+    API V2:
+
+    api/2/projects/:project_id
     <Permissions>
-    Anonymous: None.
-    Authenticated: GET/POST. PUT/PATCH/DELETE for objects owned by user.
-    Admin: GET/POST/PUT/PATCH/DELETE.
-    """
+    Anonymous: None
+    Authenticated: GET
+    Admin: None
 
-    # TODO: Implement DjangoObjectPermissions
-    queryset = Asset.objects.all()
-    serializer_class = serializers.AssetSerializer
-    permission_classes = (IsAuthenticated, DjangoObjectPermissions)
-
-
-class EventViewSet(viewsets.ModelViewSet):
-    """
-    API V2: api/2/events/:event_id
-
+    api/2/projects/:project_id/tags
     <Permissions>
-    Anonymous: None.
-    Authenticated: GET/POST
-    Admin: GET/POST
+    Anonymous: None
+    Authenticated: GET
+    Admin: None
     """
+    queryset = Project.objects.all()
+    permission_classes = (IsAuthenticated, )
 
-    # TODO: Implement ViewCreate permission.
-    queryset = Event.objects.all()
-    serializer_class = serializers.EventSerializer
-    permission_classes = (IsAuthenticated,)
-
-
-class ListenEventViewSet(viewsets.ModelViewSet):
-    """
-    API V2: api/2/listenevents/:listenevent_id
-
-    <Permissions>
-    Anonymous: None.
-    Authenticated: GET/POST.
-    Admin: GET/POST.
-    """
-
-    # TODO: Implement ViewCreate permission.
-    # TODO: Rename ListeningHistoryItem model to ListenEvent.
-    queryset = ListeningHistoryItem.objects.all()
-    serializer_class = serializers.ListenEventSerializer
-    permission_classes = (IsAuthenticated,)
-
-
-class StreamViewSet(viewsets.ViewSet):
-    """
-    The primary communication channel for handling the Roundware audio stream.
-    Only one stream per user id/token so the end point is not plural.
-    API V2: api/2/stream/
-
-    <Permissions>
-    Anonymous: None.
-    Authenticated: GET/POST/PUT/PATCH for the user specific stream.
-    Admin: GET/POST/PUT/PATCH for the user specific stream.
-    """
-    permission_classes = (IsAuthenticated,)
-
-    def list(self, request):
-        """
-        GET api/2/stream/ - Gets information about an existing stream
-        """
-        # Validate the input
-        serializer = serializers.StreamSerializer(data=request.GET)
-        if not serializer.is_valid():
-            raise ParseError(serializer.errors)
-
-        # TODO: Return data about the stream, only if it exists.
+    def retrieve(self, request, pk=None):
+        project = get_object_or_404(Project, pk=pk)
+        serializer = serializers.ProjectSerializer(project)
         return Response(serializer.data)
 
-    def create(self, request):
-        serializer = serializers.StreamSerializer()
-        return Response(serializer.data)
+    @detail_route(methods=['get'])
+    def tags(self, request, pk=None):
+        if "session_id" in request.data:
+            session = get_object_or_404(Session, pk=request.data["session_id"])
+            tags = get_project_tags(s=session)
+        else:
+            project = get_object_or_404(Project, pk=pk)
+            tags = get_project_tags(p=project)
+        return Response(tags)
 
 
-class SessionViewSet(viewsets.ModelViewSet):
+class SessionViewSet(viewsets.ViewSet):
     """
     API V2: api/2/sessions/:session_id
 
     <Permissions>
-    Anonymous: None.
+    Anonymous: None
     Authenticated: GET/POST/PUT/PATCH for objects owned by user.
-    Admin: GET/POST/PUT/PATCH.
+    Admin: None
     """
     queryset = Session.objects.all()
-    serializer_class = serializers.SessionSerializer
-    permission_classes = (IsAuthenticated, AuthenticatedReadAdminWrite)
+    permission_classes = (IsAuthenticated, )
+
+    def create(self, request):
+        serializer = serializers.SessionSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class StreamViewSet(viewsets.ViewSet):
+#     """
+#     The primary communication channel for handling the Roundware audio stream.
+#     Only one stream per user id/token so the end point is not plural.
+#     API V2: api/2/stream/
+
+#     <Permissions>
+#     Anonymous: None.
+#     Authenticated: GET/POST/PUT/PATCH for the user specific stream.
+#     Admin: GET/POST/PUT/PATCH for the user specific stream.
+#     """
+#     permission_classes = (IsAuthenticated,)
+
+#     def list(self, request):
+#         """
+#         GET api/2/stream/ - Gets information about an existing stream
+#         """
+#         # Validate the input
+#         serializer = serializers.StreamSerializer(data=request.GET)
+#         if not serializer.is_valid():
+#             raise ParseError(serializer.errors)
+
+#         # TODO: Return data about the stream, only if it exists.
+#         return Response(serializer.data)
+
+#     def create(self, request):
+#         serializer = serializers.StreamSerializer()
+#         return Response(serializer.data)
 
 
-class TagViewSet(viewsets.ModelViewSet):
-    """
-    API V2: api/2/tags/:tag_id
+# class TagViewSet(viewsets.ModelViewSet):
+#     """
+#     API V2: api/2/tags/:tag_id
 
-    <Permissions>
-    Anonymous: None.
-    Authenticated: GET.
-    Admin: GET/POST/PUT/PATCH/DELETE.
-    """
-    # TODO: Return messages and relationships in response
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
-    permission_classes = (IsAuthenticated, AuthenticatedReadAdminWrite)
+#     <Permissions>
+#     Anonymous: None.
+#     Authenticated: GET.
+#     Admin: GET/POST/PUT/PATCH/DELETE.
+#     """
+#     # TODO: Return messages and relationships in response
+#     queryset = Tag.objects.all()
+#     serializer_class = serializers.TagSerializer
+#     permission_classes = (IsAuthenticated, AuthenticatedReadAdminWrite)
 
 
 class UserViewSet(viewsets.ViewSet):
@@ -167,28 +207,3 @@ class UserViewSet(viewsets.ViewSet):
                 # obtain token for this new user
                 token = Token.objects.create(user=user)
         return Response({"username": user.username, "token": token.key})
-
-
-class ProjectViewSet(viewsets.ViewSet):
-    """
-    API V2: api/2/projects/:project_id
-            api/2/projects/:project_id/tags
-
-    <Permissions>
-    Anonymous: None
-    Authenticated: GET
-    Admin: None
-    """
-    queryset = Project.objects.all()
-    permission_classes = (IsAuthenticated, )
-
-    def retrieve(self, request, pk=None):
-        project = get_object_or_404(Project, pk=pk)
-        serializer = serializers.ProjectSerializer(project)
-        return Response(serializer.data)
-
-    @detail_route(methods=['get'])
-    def tags(self, request, pk=None):
-        project = get_object_or_404(Project, pk=pk)
-        tags = get_project_tags(p=project)
-        return Response(tags)
