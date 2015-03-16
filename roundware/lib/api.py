3bb46e2d60b5282e44530c1110f3395eae499232
Gray Bowman <gray.bowman@gmail.com>
2015-03-16 11:34:20 -0400

    Fix NotFound issues: use proper NotFound response, not ParseError, when an object is 404.
    Documented NotFound exception cannot be found. Substitute django's Http404

--- roundware/lib/api.py
+++ roundware/lib/api.py
@@ -7,6 +7,7 @@ from roundwared import gpsmixer
 from roundwared import icecast2
 from django.conf import settings
 from django.db.models import Count, Avg
+from django.http import Http404
 import datetime
 import json
 import os
@@ -669,7 +670,7 @@ def assets_by_query_params(request, project_id=None):
         try:
             project = models.Project.objects.get(pk=project_id)
         except models.Project.DoesNotExist:
-            raise ParseError("Project not found")
+            raise Http404("Project not found")
         assets = assets.filter(project=project)
 
     # optionally filter assets by session
@@ -694,7 +695,7 @@ def assets_by_query_params(request, project_id=None):
             lang = models.Language.objects.get(language_code=request.query_params["language"])
             assets = assets.filter(language=lang)
         except models.Language.DoesNotExist:
-            raise ParseError("Language not found")
+            raise Http404("Language not found")
 
     # optionally filter assets by envelope
     if "envelope_id" in request.query_params:
