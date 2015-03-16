78e0676de9b34b701edddf54e864a3a2f0b9dec9
Gray Bowman <gray.bowman@gmail.com>
2015-03-16 11:18:35 -0400

    Django required migrations to be merged

---
+++ roundware/rw/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rw', '0005_timedassets'),
+        ('rw', '0006_session_timezone'),
+    ]
+
+    operations = [
+    ]
