1b39d2b9e0bf62393fcc90f334a99e94bea07fc4
Gray Bowman <gray.bowman@gmail.com>
2015-03-19 11:26:50 -0400

    better event filter

--- roundware/api2/filters.py
+++ roundware/api2/filters.py
@@ -3,13 +3,13 @@ import django_filters
 
 
 class EventFilter(django_filters.FilterSet):
-    event_type = django_filters.CharFilter(lookup_type='startswith')
+    event_type = django_filters.CharFilter(lookup_type='icontains')
     server_time = django_filters.DateTimeFilter(lookup_type='startswith')
     server_time__lt = django_filters.DateTimeFilter(name='server_time', lookup_type='lt')
     server_time__gt = django_filters.DateTimeFilter(name='server_time', lookup_type='gt')
     session_id = django_filters.NumberFilter()
-    latitude = django_filters.NumberFilter()
-    longitude = django_filters.NumberFilter()
+    latitude = django_filters.CharFilter(lookup_type='startswith')
+    longitude = django_filters.CharFilter(lookup_type='startswith')
     tags = django_filters.CharFilter(lookup_type='icontains')
 
     class Meta:
