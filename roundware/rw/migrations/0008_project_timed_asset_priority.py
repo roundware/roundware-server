eb03e4732179f47a04c720bf611a76371be7ac09
Gray Bowman <gray.bowman@gmail.com>
2015-09-14 14:07:22 -0400

    corrected migration conflict

--- roundware/rw/migrations/0008_project_timed_asset_priority.py
+++ roundware/rw/migrations/0008_project_timed_asset_priority.py
@@ -9,6 +9,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('rw', '0007_add_rate_vote_type'),
         ('rw', '0007_repeat_mode'),
+        ('rw', '0008_add_rate_vote_type'),
     ]
 
     operations = [
