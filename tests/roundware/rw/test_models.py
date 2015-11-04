4464d0ea8118baf7cea339798ed98b10c13bbffe
yangjackascd <yangjackascd@gmail.com>
2015-11-04 18:00:30 +1300

    Revert "increase the code test"
    This reverts commit 75793c30b9b2f707a2e2d002b38a1e1d7826aa12.

--- tests/roundware/rw/test_models.py
+++ tests/roundware/rw/test_models.py
@@ -10,6 +10,7 @@ from roundware.settings import DEFAULT_SESSION_ID
 
 from .common import use_locmemcache, RWTestCase
 
+
 class TestMasterUI(RWTestCase):
 
     """ exercise MasterUI model class """
@@ -46,20 +47,7 @@ class TestMasterUI(RWTestCase):
         self.masterui.save()
         get_tag_cats = self.project.get_tag_cats_by_ui_mode(ui_mode)
         self.assertNotIn(self.masterui.tag_category, get_tag_cats)
-        
-    # Karl
-    def test_get_header_text_loc(self):
-        self.assertEquals('', self.masterui.get_header_text_loc())
-    
-    def test_toTagDictionary(self):
-        output = ""
-        sequence = ({u'code': u'fJyTlBrUQSTZgrVXcBLfcvgiMeGlGcFARIcLfiLtpBmtoVUDQh', u'name': u'ConfAnyCfUvcRGOAoFoyMKwRwaWhDUZceBehQjYHYosywLGuAr', u'select': u'single', u'order': -8309})
-        return (output.join(sequence), self.masterui.toTagDictionary())
-    
-    # Karl
-    def test___unicode__(self):
-        self.assertEquals("1:" + self.project.name + ":" + self.masterui.get_ui_mode_display() + ":" + self.masterui.name, self.masterui.__unicode__())
-        
+
 
 class TestProject(RWTestCase):
 
@@ -91,7 +79,8 @@ class TestProject(RWTestCase):
                                      ui_mode=models.MasterUI.SPEAK)
         cats = self.project.get_tag_cats_by_ui_mode(self.ui_mode)
         self.assertNotIn(other_master_ui.tag_category, cats)
-        
+
+
 class TestAsset(RWTestCase):
 
     def setUp(self):
@@ -111,164 +100,9 @@ class TestAsset(RWTestCase):
         self.vote4 = mommy.make('rw.Vote', session=self.session1,
                                 asset=self.asset1, type="flag")
 
-    
-    # Karl
-    def test_media_display(self):
-        self.assertEquals(self.asset1.audio_player(), self.asset1.media_display())
-
-
-    # Karl
-    def test_audio_player(self):
-        self.assertEquals('<div data-filename="None" class="media-display audio-file"></div>', self.asset1.audio_player())
-
-
-    # Karl
-    def test_image_display(self):
-        self.assertEquals('<div data-filename="None" class="media-display image-file"><a href="/rwmedia/None" target="imagepop"><img src="/rwmedia/None" alt="" title="click for full image"/></a></div>', self.asset1.image_display())
-
-
-    # Karl
-    def test_text_display(self):
-        self.assertEquals('<div data-filename="None" class="media-display text-file"\n'
-                          '                   >mock file\n'
-                          ' </div>', self.asset1.text_display())
-
-
-    # Karl
-    def test_location_map(self):
-        self.assertEquals('<input type="text" value="" id="searchbox" style=" width:700px;height:30px; font-size:15px;">\n        <div id="map_instructions">To change or select location, type an address above and select from the available options;\n        then move pin to exact location of asset.</div>\n        <div class="GMap" id="map" style="width:800px; height: 600px; margin-top: 10px;"></div>', self.asset1.location_map())
-
-
-    # Karl
-    def test_norm_audiolength(self):
-        self.assertEquals(None, self.asset1.norm_audiolength())
-        self.assertEquals(self.asset1.audiolength, self.asset1.norm_audiolength())
-        self.assertEquals(None, self.asset2.norm_audiolength())
-        self.assertEquals(self.asset2.audiolength, self.asset2.norm_audiolength())
-
-
-    # Karl
-    def test_media_link_url(self):
-        self.assertEquals('<a href="/rwmedia/None" target="_new">None</a>', self.asset1.media_link_url())
-    
-    # Karl
-    def test_get_tags(self):
-        self.assertEquals("", self.asset1.get_tags())
-
     def test_get_likes(self):
         self.assertEquals(2, self.asset1.get_likes())
         self.assertEquals(1, self.asset2.get_likes())
 
     def test_get_flags(self):
         self.assertEquals(1, self.asset1.get_flags())
-        
-    # Karl
-    def test_get_votes(self):
-        self.assertEquals("flag : 0, like : 0", self.asset1.get_votes())
-
-        
-class TestLocalizedString(RWTestCase):
-    def setUp(self):
-        super(type(self), TestLocalizedString).setUp(self)
-        self.localized_string = mommy.make('rw.LocalizedString')
-        
-    def test___unicode__(self):
-        self.assertEquals('1: Language Code: ' + str(self.localized_string.language.language_code) + ', String: ' + str(self.localized_string.localized_string), self.localized_string.__unicode__())
-        
-# Karl
-class TestSession(RWTestCase):
-    # Karl
-    def setUp(self):
-        super(type(self), TestSession).setUp(self)
-        self.project = mommy.make('rw.Session')
-
-    # Karl
-    def test___unicode__(self):
-        self.assertEquals('1', self.project.__unicode__())
-
-class TestTag(RWTestCase):
-    def setUp(self):
-        super(type(self), TestTag).setUp(self)
-        self.project = mommy.make('rw.Tag')
-    
-    def test_get_loc(self):
-        self.assertEquals('', self.project.get_loc())
-    
-    def test___unicode__(self):
-        self.assertEquals(self.project.tag_category.name + " : " + self.project.value, self.project.__unicode__())
-
-class TestUIMapping(RWTestCase):
-    
-    def setUp(self):
-        super(type(self), TestUIMapping).setUp(self)
-        self.master_ui = mommy.make('rw.MasterUI')
-        self.project = mommy.make('rw.UIMapping')
-
-    def test___unicode__(self):
-        self.assertEquals("1:" + self.project.master_ui.name + ":" + self.project.tag.tag_category.name, self.project.__unicode__())
-
-class TestAudiotrack(RWTestCase):
-    
-    def setUp(self):
-        super(type(self), TestAudiotrack).setUp(self)
-        self.project = mommy.make('rw.Audiotrack')
-    
-    def test_norm_minduration(self):
-        if (self.project.norm_minduration() == '0.00 s'):
-            self.assertEquals('0.00 s', self.project.norm_minduration())
-        elif (self.project.norm_minduration() == '-0.00 s'):
-            self.assertEquals('-0.00 s', self.project.norm_minduration())
-    
-    def test_norm_maxduration(self):
-        if (self.project.norm_maxduration() == '0.00 s'):
-            self.assertEquals('0.00 s', self.project.norm_maxduration())
-        elif (self.project.norm_maxduration() == '-0.00 s'):
-            self.assertEquals('-0.00 s', self.project.norm_maxduration())
-
-    def test_norm_mindeadair(self):
-        if (self.project.norm_mindeadair() == '0.00 s'):
-            self.assertEquals('0.00 s', self.project.norm_mindeadair())
-        elif (self.project.norm_mindeadair() == '-0.00 s'):
-            self.assertEquals('-0.00 s', self.project.norm_mindeadair())
-
-    def test_norm_maxdeadair(self):
-        if (self.project.norm_maxdeadair() == '0.00 s'):
-            self.assertEquals('0.00 s', self.project.norm_maxdeadair())
-        elif (self.project.norm_maxdeadair() == '0.00 s'):
-            self.assertEquals('-0.00 s', self.project.norm_maxdeadair())
-
-    def test___unicode__(self):
-        self.assertEquals('Track 1', self.project.__unicode__())
-        
-class TestEnvelope(RWTestCase):
-    def setUp(self):
-        super(type(self), TestEnvelope).setUp(self)
-        self.project = mommy.make('rw.Envelope')
-
-
-        
-class TestSpeaker(RWTestCase):
-    def setUp(self):
-        super(type(self), TestSpeaker).setUp(self)
-        self.project = mommy.make('rw.Speaker')
-    
-    def test___unicode__(self):
-        self.assertEquals('1: ' + str(self.project.latitude) + "/" + str(self.project.longitude)  + " : " + str(self.project.uri), self.project.__unicode__())
-    
-    def test_location_map(self):
-        self.assertEquals('<input type="text" value="" id="searchbox" style=" width:700px;height:30px; font-size:15px;">\n        <div id="map_instructions">To change or select location, type an address above and select from the available options;\n        then move pin to exact location of asset.</div>\n        <div class="GMap" id="map" style="width:800px; height: 600px; margin-top: 10px;"></div>', self.project.location_map())
-
-
-class TestListeningHistoryItem(RWTestCase):
-    def setUp(self):
-        super(type(self), TestListeningHistoryItem).setUp(self)
-        self.project = mommy.make('rw.ListeningHistoryItem')
-    
-    def test_norm_duration(self):
-        self.assertEquals(None, self.project.norm_duration())
-    
-    def test_duration_in_seconds(self):
-        self.assertEquals(None, self.project.duration_in_seconds())
-
-    def test__unicode__(self):
-        self.assertEquals('1: Session id: 1: Asset id: 1 duration: None', self.project.__unicode__())
