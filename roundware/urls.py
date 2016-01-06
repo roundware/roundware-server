ad9f3898b0c02d69518a5f31e80ad25b8ff0efd9
Halsey Burgund <halsey@halseyburgund.com>
2016-01-06 16:15:40 -0500

    minor updates to Asset Edit map to make consistent with Listen Map

--- roundware/urls.py
+++ roundware/urls.py
@@ -20,7 +20,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^dashboard/asset-map$', 'rw.views.asset_map'),
+    url(r'^tools/asset-map$', 'rw.views.asset_map'),
     url(r'^tools/listen-map$', 'rw.views.listen_map'),
     url(r'^dashboard/$', 'rw.views.chart_views'),
 
